17c06d23eded34b9e19b4fd74d91376d2fc861e0
adeelamanat566 <adeelamanat566@gmail.com>
2026-09-01 16:44:32 +0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL
+    phone VARCHAR(20),
+    address VARCHAR(255);
 );
 
 
